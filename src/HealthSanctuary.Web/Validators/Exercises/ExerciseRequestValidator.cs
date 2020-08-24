0d1eade4ae195580c85f5ac405f4c64529ce4182
SirKaelan <kaelanfreakerman6@gmail.com>
2020-08-24 21:24:59 +0300

    Add ExerciseRequest endpoint validations

---
+++ src/HealthSanctuary.Web/Validators/Exercises/ExerciseRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using HealthSanctuary.Web.Models.Exercises;
+
+namespace HealthSanctuary.Web.Validators.Exercises
+{
+    public class ExerciseRequestValidator : AbstractValidator<ExerciseRequest>
+    {
+        public ExerciseRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Description).MaximumLength(500);
+            RuleFor(x => x.VideoLink).MaximumLength(100);
+        }
+    }
+}
--- src/HealthSanctuary.Web/Validators/Workouts/WorkoutRequestValidator.cs
+++ src/HealthSanctuary.Web/Validators/Workouts/WorkoutRequestValidator.cs
@@ -8,8 +8,8 @@ namespace HealthSanctuary.Web.Validators.Workouts
         public WorkoutRequestValidator()
         {
             RuleFor(x => x.Title).NotEmpty().MaximumLength(50);
-            RuleFor(x => x.Description).MaximumLength(500).When(x => x.Description != null);
-            RuleFor(x => x.VideoLink).MaximumLength(100).When(x => x.VideoLink != null);
+            RuleFor(x => x.Description).MaximumLength(500);
+            RuleFor(x => x.VideoLink).MaximumLength(100);
             RuleFor(x => x.Duration).GreaterThan(0);
             RuleFor(x => x.WorkoutExercises).NotEmpty();
             RuleForEach(x => x.WorkoutExercises).SetValidator(new WorkoutExerciseRequestValidator());
